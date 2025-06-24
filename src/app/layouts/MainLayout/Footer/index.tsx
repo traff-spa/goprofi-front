9def821d6fd5c796eb0f75a544223a2a7562e572
Евгений Богук <yevhenii@MacBook-Pro-Evgenij.local>
2025-06-24 20:26:02 +0200

    add privacy-policy page

--- src/app/layouts/MainLayout/Footer/index.tsx
+++ src/app/layouts/MainLayout/Footer/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+import { ROUTES } from '@/app/routes/paths';
+
 import '@/app/styles/footer.scss';
 
 export const Footer = () => (
     <footer className="footer">
         <div className="footer__inner">
-            <a href="#">Політика конфіденційності</a>
+            <Link to={ROUTES.POLICY}>
+                Політика конфіденційності
+            </Link>
             <a href="#">Договір оферти</a>
         </div>
     </footer>
--- src/app/routes/paths.ts
+++ src/app/routes/paths.ts
@@ -4,5 +4,6 @@ export const ROUTES = {
   PROFILE: '/profile',
   TEST_HISTORY: '/test-history',
   TEST_PAGE: '/test/:id',
-  ADMIN: '/admin'
+  ADMIN: '/admin',
+  POLICY: '/privacy-policy'
 } as const;
